847a01d05b82bd2b32822537d34bb734f34ad0d6
Santiago3896 <santigini96@gmail.com>
2023-10-30 23:56:41 -0300

    Pasado boostrap

---
+++ inicio/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from inicio.views import inicio, paletas
+
+urlpatterns = [
+    path('', inicio, name = "inicio"),
+    path('paletas/', paletas, name = "paletas")
+]
--- inicio/views.py
+++ inicio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
+# from django.template import loader
+# from django.http import HttpResponse
+from inicio.models import Paleta
 
 
 def inicio(request):
@@ -14,4 +15,10 @@ def inicio(request):
     
     # v3
     
-    return render(request,"inicio.html", {})
+    return render(request,"inicio/inicio.html", {})
+
+def paletas(request):
+    paleta = Paleta(marca = "Babolat", descripcion = "Paleta de primera marca", anio = 2023)
+    paleta.save()
+    
+    return render(request, "inicio/paletas.html", {'paleta': paleta})
