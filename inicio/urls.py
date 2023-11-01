2ddc6b2eb8d2c97d35933713b1b6ea823e53a1df
Santiago3896 <santigini96@gmail.com>
2023-11-01 19:07:17 -0300

    Forms_creacion

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from inicio.views import inicio, paletas , alquilar_paletas, alquilar_canchas, alquilar_pelotitas
+from inicio.views import inicio , paletas, alquilar_paletas, alquilar_canchas, alquilar_pelotitas
 
 urlpatterns = [
     path('', inicio, name = "inicio"),
-    path('paletas/', paletas, name = "paletas"),
-    path('paletas/alquilar', alquilar_paletas , name = "alquilar_paleta"),
-    path('canchas/alquilar', alquilar_canchas , name = "alquilar_paleta"),
-    path('pelotitas/alquilar', alquilar_pelotitas , name = "alquilar_paleta")  
+    path('paletas/alquiler', alquilar_paletas , name = "alquiler_paleta"),
+    path('canchas/alquiler', alquilar_canchas , name = "alquilar_canchas"),
+    path('pelotitas/alquiler', alquilar_pelotitas , name = "alquilar_pelotitas")  
 ]
